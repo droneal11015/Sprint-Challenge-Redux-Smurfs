d416ea9c42f02e90c33fe6c28aa63fba60b7677f
droneal11015 <droneal11015@gmail.com>
2019-05-24 12:03:13 -0400

    integrated previous files for functionality

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+
+
+class SmurfForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: '',
+      height: ''
+    };
+  }
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios
+      .post('http://localhost:3333/smurfs', newSmurf)
+      .then(res => {
+        console.log(res.data);
+       this.props.onSubmit(res.data);
+       
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="SmurfForm">
+        <form onSubmit={this.addSmurf}>
+          <input
+            onChange={this.handleInputChange}
+            placeholder="name"
+            value={this.state.name}
+            name="name"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="age"
+            value={this.state.age}
+            name="age"
+          />
+          <input
+            onChange={this.handleInputChange}
+            placeholder="height in cm"
+            value={this.state.height}
+            name="height"
+          />
+          <button type="submit">Add to the village</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SmurfForm;
